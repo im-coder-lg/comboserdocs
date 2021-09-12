167c2419593c56be455bef6bde4ad664116f23fb
im-coder-lg <76845820+im-coder-lg@users.noreply.github.com>
2021-09-12 07:29:57 +0000

    Deploying to gh-pages from  @ 9d33a609797b4493f2914d03ffbf959c9f52432a 🚀

---
+++ codedoc-bundle.entry.js
@@ -0,0 +1,42 @@
+import { getRenderer } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/transport/renderer.js';
+import { initJssCs } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/transport/setup-jss.js';initJssCs();
+import { installTheme } from '/home/runner/work/docs/docs/.codedoc/content/theme.ts';installTheme();
+import { codeSelection } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/selection.js';codeSelection();
+import { sameLineLengthInCodes } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/same-line-length.js';sameLineLengthInCodes();
+import { initHintBox } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-hint/index.js';initHintBox();
+import { initCodeLineRef } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-ref/index.js';initCodeLineRef();
+import { initSmartCopy } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/smart-copy.js';initSmartCopy();
+import { copyHeadings } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/heading/copy-headings.js';copyHeadings();
+import { contentNavHighlight } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/contentnav/highlight.js';contentNavHighlight();
+import { loadDeferredIFrames } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/transport/deferred-iframe.js';loadDeferredIFrames();
+import { smoothLoading } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/transport/smooth-loading.js';smoothLoading();
+import { tocHighlight } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toc-highlight.js';tocHighlight();
+import { postNavSearch } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/search/post-nav/index.js';postNavSearch();
+import { copyLineLinks } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/code/line-links/copy-line-link.js';copyLineLinks();
+import { gatherFootnotes } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/footnote/gather-footnotes.js';gatherFootnotes();
+import { ToCPrevNext } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/prevnext/index.js';
+import { GithubSearch } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/misc/github/search.js';
+import { ToCToggle } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/page/toc/toggle/index.js';
+import { GitterToggle } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/misc/gitter/index.js';
+import { DarkModeSwitch } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/components/darkmode/index.js';
+import { ConfigTransport } from '/home/runner/work/docs/docs/.codedoc/node_modules/@codedoc/core/dist/es6/transport/config.js';
+
+const components = {
+  'h7smMF/VsWVW7P5N7MOSNA==': ToCPrevNext,
+  'PfXATFqFr23OoTubEQt6vQ==': GithubSearch,
+  'x7C0LCI6paKTeYeOkoEYMQ==': ToCToggle,
+  'EVFhS5IuxXlO3n5MuOf4wQ==': GitterToggle,
+  'ydOT30OH8OhdZRvGCPz8BQ==': DarkModeSwitch,
+  'kL9atIC7mpqcSjBja2l5iw==': ConfigTransport
+};
+
+const renderer = getRenderer();
+const ogtransport = window.__sdh_transport;
+window.__sdh_transport = function(id, hash, props) {
+  if (hash in components) {
+    const target = document.getElementById(id);
+    renderer.render(renderer.create(components[hash], props)).after(target);
+    target.remove();
+  }
+  else if (ogtransport) ogtransport(id, hash, props);
+}
