1591b5865197a3431f6c92f3e10a49b44d779936
LG <76845820+im-coder-lg@users.noreply.github.com>
2021-09-14 15:08:37 +0530

    Update config.ts

--- .codedoc/config.ts
+++ .codedoc/config.ts
@@ -7,11 +7,9 @@ import { theme } from './theme';
 export const config = /*#__PURE__*/configuration({
   theme,                                  // --> add the theme. modify `./theme.ts` for changing the theme.
   dest: {
-    namespace: '',                 // --> your github pages namespace. remove if you are using a custom domain.
-    // html: '.',                           // --> the base folder for HTML files
-    // assets: './docs/assets',                         // --> the base folder for assets
-    // bundle: './docs/assets',               // --> where to store codedoc's bundle (relative to `assets`)
-    // styles: './docs/assets',               // --> where to store codedoc's styles (relative to `assets`)
+    namespace: '/docs',                 // --> your github pages namespace. remove if you are using a custom domain.
+    html: 'dist',
+    assets: 'dist',
     
   },
   page: {
